e774300e2c56a58027e5c340e8e30ad18c3db588
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-06-29 23:59:52 +0300

    docstrings

--- deap_er/tools/mutation.py
+++ deap_er/tools/mutation.py
@@ -24,11 +24,16 @@
 #                                                                                        #
 # ====================================================================================== #
 from deap_er._deprecated import deprecated
-from collections.abc import Sequence
+from typing import Union, Sequence
 from itertools import repeat
 import random
+import numpy
+import array
 import math
 
+
+ValueSeq = Union[int, float, Sequence]
+Entity = Union[list, numpy.ndarray, array.array]
 
 __all__ = [
     'mut_gaussian', 'mutGaussian',
@@ -45,38 +50,72 @@ def _check_bounds(individual, var1, var1_str, var2, var2_str) -> tuple:
     err_msg = '{0} must be at least the size of individual: {1} < {2}'
     size = len(individual)
 
-    if not isinstance(var1, Sequence):
+    if isinstance(var1, Sequence):
+        if len(var1) < size:
+            msg = err_msg.format(var1_str, len(var1), size)
+            raise IndexError(msg)
+    else:
         var1 = repeat(var1, size)
-    elif len(var1) < size:
-        msg = err_msg.format(var1_str, len(var1), size)
-        raise IndexError(msg)
 
-    if not isinstance(var2, Sequence):
+    if isinstance(var2, Sequence):
+        if len(var2) < size:
+            msg = err_msg.format(var2_str, len(var2), size)
+            raise IndexError(msg)
+    else:
         var2 = repeat(var2, size)
-    elif len(var2) < size:
-        msg = err_msg.format(var2_str, len(var2), size)
-        raise IndexError(msg)
 
     return var1, var2
 
 
 # -------------------------------------------------------------------------------------- #
-def mut_gaussian(individual, mu, sigma, ind_pb) -> tuple:
+def mut_gaussian(individual: Entity, mu: ValueSeq,
+                 sigma: ValueSeq, mut_prob: float) -> Entity:
+    """
+    This function applies a gaussian mutation of mean *mu* and standard
+    deviation *sigma* on the input individual. This mutation expects the
+    *individual* to be a type of *sequence*, composed of real valued attributes.
+    The *ind_pb* argument is the probability of each attribute to be mutated.
+
+    :param individual: Individual to be mutated.
+    :param mu: Mean or sequence of means for the gaussian addition mutation.
+    :param sigma: Standard deviation or sequence of standard
+        deviations for the gaussian addition mutation.
+    :param mut_prob: Probability for each attribute to be mutated.
+    :returns: A mutated individual.
+    """
     mu, sigma = _check_bounds(individual, mu, 'mu', sigma, 'sigma')
 
     for i, m, s in zip(list(range(len(individual))), mu, sigma):
-        if random.random() < ind_pb:
+        if random.random() < mut_prob:
             individual[i] += random.gauss(m, s)
 
-    return individual,
+    return individual
 
 
 # -------------------------------------------------------------------------------------- #
-def mut_polynomial_bounded(individual, eta, low, up, indpb):
+def mut_polynomial_bounded(individual: Entity, eta: float,
+                           low: ValueSeq, up: ValueSeq, mut_prob: float) -> Entity:
+    """
+    This function applies a polynomial mutation with a crowding degree of
+    *eta* on the input individual. This mutation expects the *individual*
+    to be a type of *sequence*, composed of real valued attributes. The
+    *ind_pb* argument is the probability of each attribute to be mutated.
+
+    :param individual: Individual to be mutated.
+    :param eta: Crowding degree of the mutation. Higher eta will produce
+        a mutant more similar to its parent, while a smaller eta will
+        produce a mutant more divergent from its parent.
+    :param low: A value or a sequence of values that
+        is the lower bound of the search space.
+    :param up: A value or a sequence of values that
+        is the upper bound of the search space.
+    :param mut_prob: Probability for each attribute to be mutated.
+    :returns: A mutated individual.
+    """
     low, up = _check_bounds(individual, low, 'low', up, 'up')
 
     for i, xl, xu in zip(list(range(len(individual))), low, up):
-        if random.random() <= indpb:
+        if random.random() <= mut_prob:
             x = individual[i]
             delta_1 = (x - xl) / (xu - xl)
             delta_2 = (xu - x) / (xu - xl)
@@ -96,45 +135,92 @@ def mut_polynomial_bounded(individual, eta, low, up, indpb):
             x = min(max(x, xl), xu)
             individual[i] = x
 
-    return individual,
+    return individual
 
 
 # -------------------------------------------------------------------------------------- #
-def mut_shuffle_indexes(individual, ind_pb):
+def mut_shuffle_indexes(individual: Entity, mut_prob: float) -> Entity:
+    """
+    Shuffles the attributes of the input individual. This mutation expects
+    the *individual* to be a type of *sequence* and is usually applied
+    to a vector of indices. The *ind_pb* argument is the probability of
+    each attribute to be moved.
+
+    :param individual: Individual to be mutated.
+    :param mut_prob: Probability for each attribute to be mutated.
+    :returns: A mutated individual.
+    """
     size = len(individual)
     for i in range(size):
-        if random.random() < ind_pb:
+        if random.random() < mut_prob:
             swap_indx = random.randint(0, size - 2)
             if swap_indx >= i:
                 swap_indx += 1
             individual[i], individual[swap_indx] = \
                 individual[swap_indx], individual[i]
 
-    return individual,
+    return individual
 
 
 # -------------------------------------------------------------------------------------- #
-def mut_flip_bit(individual, ind_pb) -> tuple:
+def mut_flip_bit(individual: Entity, mut_prob: float) -> Entity:
+    """
+    Flips the values of random attributes of the input individual.
+    This mutation expects the *individual* to be a type of *sequence*
+    of boolean values. The *ind_pb* argument is the probability of
+    each attribute to be flipped.
+
+    :param individual: Individual to be mutated.
+    :param mut_prob: Probability for each attribute to be mutated.
+    :returns: A mutated individual.
+    """
     for i in range(len(individual)):
-        if random.random() < ind_pb:
+        if random.random() < mut_prob:
             individual[i] = type(individual[i])(not individual[i])
 
-    return individual,
+    return individual
 
 
 # -------------------------------------------------------------------------------------- #
-def mut_uniform_int(individual, low, up, ind_pb) -> tuple:
+def mut_uniform_int(individual: Entity,
+                    low: int, up: int,
+                    mut_prob: float) -> Entity:
+    """
+    Mutates an individual by replacing attribute values with integers
+    chosen uniformly between *low* and *up* inclusively. This mutation
+    expects the *individual* to be a type of *sequence*. The *ind_pb*
+    argument is the probability of each attribute to be mutated.
+
+    :param individual: Individual to be mutated.
+    :param low: Lower bound of the range of integers.
+    :param up: Upper bound of the range of integers.
+    :param mut_prob: Probability for each attribute to be mutated.
+    :returns: A mutated individual.
+    """
     low, up = _check_bounds(individual, low, 'low', up, 'up')
 
     for i, xl, xu in zip(list(range(len(individual))), low, up):
-        if random.random() < ind_pb:
+        if random.random() < mut_prob:
             individual[i] = random.randint(xl, xu)
 
-    return individual,
+    return individual
 
 
 # -------------------------------------------------------------------------------------- #
-def mut_es_log_normal(individual, c, ind_pb):
+def mut_es_log_normal(individual: Entity,
+                      c: float, mut_prob: float) -> Entity:
+    """
+    Mutates an evolution strategy according to its *strategy* attribute.
+    This mutation expects the *individual* to be a type of *sequence*.
+    The *c* argument is the learning rate and the *ind_pb* argument
+    is the probability of each attribute to be mutated.
+
+    :param individual: Individual to be mutated.
+    :param c: Learning rate. The recommended value is 1
+        when using a (10, 100) evolution strategy.
+    :param mut_prob: Probability for each attribute to be mutated.
+    :returns: A mutated individual.
+    """
     size = len(individual)
     t = c / math.sqrt(2. * math.sqrt(size))
     t0 = c / math.sqrt(2. * size)
@@ -142,11 +228,12 @@ def mut_es_log_normal(individual, c, ind_pb):
     t0_n = t0 * n
 
     for indx in range(size):
-        if random.random() < ind_pb:
-            individual.strategy[indx] *= math.exp(t0_n + t * random.gauss(0, 1))
-            individual[indx] += individual.strategy[indx] * random.gauss(0, 1)
+        if random.random() < mut_prob:
+            if hasattr(individual, 'strategy'):
+                individual.strategy[indx] *= math.exp(t0_n + t * random.gauss(0, 1))
+                individual[indx] += individual.strategy[indx] * random.gauss(0, 1)
 
-    return individual,
+    return individual
 
 
 # -------------------------------------------------------------------------------------- #
