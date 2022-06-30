f7b1a519be9fcca1d4426debf057ac25444baa42
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-06-30 13:29:32 +0300

    docstrings

--- deap_er/tools/migration.py
+++ deap_er/tools/migration.py
@@ -24,28 +24,48 @@
 #                                                                                        #
 # ====================================================================================== #
 from deap_er._deprecated import deprecated
+from typing import Union, Callable
+import numpy
+import array
 
 
+Sequence = Union[list, numpy.ndarray, array.array]
 __all__ = ['migRing']
 
 
 # ====================================================================================== #
-def mig_ring(populations, k, selection, replacement=None, mig_array=None):
-    nbr_demes = len(populations)
-    if mig_array is None:
-        mig_array = list(range(1, nbr_demes)) + [0]
+def mig_ring(populations: Sequence, mig_count: int, selection: Callable,
+             replacement: Callable = None, mig_indices: Sequence = None) -> None:
+    """
+    Performs a ring migration between the *populations*. The migration
+    first selects *mig_count* emigrants from each population using the
+    specified *selection* operator and then switches the selected
+    individuals between the populations.
 
-    immigrants = [[] for i in range(nbr_demes)]
-    emigrants = [[] for i in range(nbr_demes)]
+    :param populations: A list of (sub-)populations on which to operate migration.
+    :param mig_count: The number of individuals to migrate.
+    :param selection: The function to select emigrants from each population.
+    :param replacement: The function to select which individuals will be switched.
+    :param mig_indices: A list of indices indicating where the individuals from
+        a particular position in the list goes. This defaults to a ring migration.
+    :returns: None
+    """
+    nbr_demes = len(populations)
+    if mig_indices is None:
+        mig_indices = list(range(1, nbr_demes)) + [0]
+
+    immigrants = [[] for _ in range(nbr_demes)]
+    emigrants = [[] for _ in range(nbr_demes)]
 
     for from_deme in range(nbr_demes):
-        emigrants[from_deme].extend(selection(populations[from_deme], k))
+        emigrants[from_deme].extend(selection(populations[from_deme], mig_count))
         if replacement is None:
             immigrants[from_deme] = emigrants[from_deme]
         else:
-            immigrants[from_deme].extend(replacement(populations[from_deme], k))
+            emigrants = replacement(populations[from_deme], mig_count)
+            immigrants[from_deme].extend(emigrants)
 
-    for from_deme, to_deme in enumerate(mig_array):
+    for from_deme, to_deme in enumerate(mig_indices):
         for i, immigrant in enumerate(immigrants[to_deme]):
             indx = populations[to_deme].index(immigrant)
             populations[to_deme][indx] = emigrants[from_deme][i]
