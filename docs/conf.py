3a2a8d810448571a66fdad83cba28f38c6c67380
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-07-16 23:18:52 +0300

    rtd fix

--- docs/conf.py
+++ docs/conf.py
@@ -9,11 +9,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath("../../"))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -23,7 +21,7 @@ copyright = '2022, Mattias Aabmets'
 author = 'Mattias Aabmets'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
