8a2b10088fa32f32143544584aef6df1ac708300
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-07-16 23:22:10 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,9 +9,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -41,6 +41,11 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+# Help out autodoc
+autodoc_mock_imports = [
+    'numpy'
+]
 
 
 # -- Options for HTML output -------------------------------------------------
