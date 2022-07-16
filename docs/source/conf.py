2986fe16d2295a0de7aeb0712427727f99ff8e40
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-07-16 22:51:01 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../deap_er'))
+sys.path.insert(0, os.path.abspath('../deap-er'))
 
 
 # -- Project information -----------------------------------------------------
