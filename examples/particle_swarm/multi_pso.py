54626f5651d2e6c81b7a91b134820417575c6e4c
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-07-31 18:51:12 +0300

    Update multi_pso.py

--- examples/particle_swarm/multi_pso.py
+++ examples/particle_swarm/multi_pso.py
@@ -10,8 +10,10 @@ import numpy
 import math
 
 
-random.seed(1234)  # disables randomization
+# Disable randomization to guarantee reproducibility
+random.seed(1234)
 
+# Define constants, objects and functions.
 NDIM = 5
 NSWARMS = 1
 NPARTICLES = 5
@@ -23,22 +25,37 @@ AVG_OE_THRESHOLD = 5
 VERBOSE = True
 
 SCENARIO = evals.MPConfigs.ALT1
+mpb = evals.MovingPeaks(dimensions=NDIM, **SCENARIO)
+
 BOUNDS = [SCENARIO["min_coord"], SCENARIO["max_coord"]]
 SMIN = -(BOUNDS[1] - BOUNDS[0]) / 2.0
 SMAX = (BOUNDS[1] - BOUNDS[0]) / 2.0
 PMIN = BOUNDS[0]
 PMAX = BOUNDS[1]
 
-mpb = evals.MovingPeaks(dimensions=NDIM, **SCENARIO)
 
-
-def generate(pclass, dim, pmin, pmax, smin, smax):
+def generate_particle(pclass, dim, pmin, pmax, smin, smax):
     part = pclass(random.uniform(pmin, pmax) for _ in range(dim)) 
     part.speed = [random.uniform(smin, smax) for _ in range(dim)]
     return part
 
 
-def convert(swarm, rcloud, centre, dist):
+def update_particle(part, best, chi, c):
+    ce1 = (c * random.uniform(0, 1) for _ in range(len(part)))
+    ce2 = (c * random.uniform(0, 1) for _ in range(len(part)))
+    ce1_p = map(operator.mul, ce1, map(operator.sub, best, part))
+    ce2_g = map(operator.mul, ce2, map(operator.sub, part.best, part))
+    a = map(
+        operator.sub, map(
+            operator.mul, itertools.repeat(chi), map(
+                operator.add, ce1_p, ce2_g)
+        ), map(operator.mul, itertools.repeat(1 - chi), part.speed)
+    )
+    part.speed = list(map(operator.add, part.speed, a))
+    part[:] = list(map(operator.add, part, part.speed))
+
+
+def convert_swarm(swarm, rcloud, centre, dist):
     dim = len(swarm[0])
     for part in swarm:
         position = [random.gauss(0, 1) for _ in range(dim)]
@@ -61,21 +78,6 @@ def convert(swarm, rcloud, centre, dist):
     return swarm
 
 
-def update(part, best, chi, c):
-    ce1 = (c * random.uniform(0, 1) for _ in range(len(part)))
-    ce2 = (c * random.uniform(0, 1) for _ in range(len(part)))
-    ce1_p = map(operator.mul, ce1, map(operator.sub, best, part))
-    ce2_g = map(operator.mul, ce2, map(operator.sub, part.best, part))
-    a = map(
-        operator.sub, map(
-            operator.mul, itertools.repeat(chi), map(
-                operator.add, ce1_p, ce2_g)
-        ), map(operator.mul, itertools.repeat(1 - chi), part.speed)
-    )
-    part.speed = list(map(operator.add, part.speed, a))
-    part[:] = list(map(operator.add, part, part.speed))
-
-
 def setup():
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Particle", list, fitness=creator.FitnessMax,
@@ -83,11 +85,11 @@ def setup():
     creator.create("Swarm", list, best=None, bestfit=creator.FitnessMax)
 
     toolbox = base.Toolbox()
-    toolbox.register("particle", generate, creator.Particle,
+    toolbox.register("particle", generate_particle, creator.Particle,
                      dim=NDIM, pmin=PMIN, pmax=PMAX, smin=SMIN, smax=SMAX)
     toolbox.register("swarm", utils.init_repeat, creator.Swarm, toolbox.particle)
-    toolbox.register("update", update, chi=0.729843788, c=2.05)
-    toolbox.register("convert", convert, dist=SWARM_DSTRB)
+    toolbox.register("update", update_particle, chi=0.729843788, c=2.05)
+    toolbox.register("convert", convert_swarm, dist=SWARM_DSTRB)
     toolbox.register("evaluate", mpb)
 
     stats = records.Statistics(lambda ind: ind.fitness.values)
@@ -102,17 +104,18 @@ def setup():
     return toolbox, stats, logbook
 
 
-def avg_offline_error(logbook):
+def stop_condition(logbook):
     interval = AVG_OE_MEASURE_INTERVAL
-    if len(logbook) % interval != 0:
-        return AVG_OE_THRESHOLD + 1
-    else:
+    if len(logbook) % interval == 0:
         err_sum = 0
         for i in range(interval, 0, -1):
             val = logbook.select("offline_error")[-i]
             err_sum += val
-        avg = err_sum / interval
-        return avg
+        avg_err = err_sum / interval
+        if avg_err <= AVG_OE_THRESHOLD:
+            print_results(avg_err)
+            return 1
+    return 0
 
 
 def print_results(avg_err):
@@ -125,7 +128,7 @@ def print_results(avg_err):
 def main():
     toolbox, stats, logbook = setup()
 
-    def eval_fitness(group):
+    def update_fitness(group):
         part.fitness.values = toolbox.evaluate(part)
         if not part.best or part.fitness > part.bestfit:
             part.best = toolbox.clone(part[:])
@@ -134,7 +137,7 @@ def main():
             group.best = toolbox.clone(part[:])
             group.bestfit.values = part.fitness.values
 
-    def log_stats(ngen):
+    def log_stats(ngen=0):
         record = stats.compile(list(itertools.chain(*population)))
         args = dict(
             gen=ngen,
@@ -147,22 +150,29 @@ def main():
         if VERBOSE:
             print(logbook.stream)
 
+    # Generate the initial population.
     population = [toolbox.swarm(size=NPARTICLES) for _ in range(NSWARMS)]
+
+    # Evaluate the initial population.
     for swarm in population:
         for part in swarm:
-            eval_fitness(swarm)
+            update_fitness(swarm)
 
-    log_stats(0)
+    log_stats()
 
     generation = 1
-    avg_err = AVG_OE_THRESHOLD + 1
 
-    while avg_err > AVG_OE_THRESHOLD:
+    # Define the main evolution loop.
+    while not stop_condition(logbook):
+
+        # Reset convergence variables.
         rex_cl = (BOUNDS[1] - BOUNDS[0]) / (2 * len(population) ** (1.0 / NDIM))
         worst_swarm_idx = None
         worst_swarm = None
         not_converged = 0
 
+        # Compute the diameters of the swarms and search for the
+        # worst swarm according to its best global position.
         for i, swarm in enumerate(population):
             for p1, p2 in itertools.combinations(swarm, 2):
                 d = math.sqrt(sum((x1 - x2) ** 2. for x1, x2 in zip(p1, p2)))
@@ -173,24 +183,36 @@ def main():
                         worst_swarm = swarm
                     break
 
+        # If all swarms have converged, add a swarm.
         if not_converged == 0:
             population.append(toolbox.swarm(size=NPARTICLES))
+
+        # If too many swarms are roaming, remove the worst swarm.
         elif not_converged > NEXCESS:
             population.pop(worst_swarm_idx)
 
+        # Update and evaluate the swarm.
         for swarm in population:
+
+            # Check for change.
             if swarm.best and toolbox.evaluate(swarm.best) != swarm.bestfit.values:
+
+                # Convert particles to quantum particles.
                 swarm[:] = toolbox.convert(swarm, rcloud=RCLOUD, centre=swarm.best)
                 swarm.best = None
                 del swarm.bestfit.values
 
+            # Not necessary to update if it's a new swarm
+            # or a swarm just converted to quantum.
             for part in swarm:
                 if swarm.best and part.best:
                     toolbox.update(part, swarm.best)
-                eval_fitness(swarm)
+                update_fitness(swarm)
 
         log_stats(generation)
 
+        # Apply exclusion to swarms which have the best
+        # position and are not set to reinitialize.
         reinit_swarms = set()
         for s1, s2 in itertools.combinations(range(len(population)), 2):
             if (
@@ -207,15 +229,14 @@ def main():
                     else:
                         reinit_swarms.add(s2)
 
+        # Reinitialize and evaluate swarms.
         for s in reinit_swarms:
             population[s] = toolbox.swarm(size=NPARTICLES)
             for part in population[s]:
-                eval_fitness(population[s])
+                update_fitness(population[s])
 
-        avg_err = avg_offline_error(logbook)
+        # Update iteration counter.
         generation += 1
-
-    print_results(avg_err)
 
 
 if __name__ == "__main__":
