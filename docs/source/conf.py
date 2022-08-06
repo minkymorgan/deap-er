243171beb760f257df6bc439bf987d0ec5715b53
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-08-06 16:38:24 +0300

    rtd fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ add_module_names = False
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-pygments_style = 'material'
+pygments_style = '_static.material.MaterialStyle'
 highlight_language = 'python3'
 html_static_path = ['_static']
 
